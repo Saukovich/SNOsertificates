b41a2a34fa5d5253bff110f22c6747923df6d2fc
Misha_Nya <misha.nya.narf@gmail.com>
2025-11-27 23:16:54 +0300

    add inline-keyboard

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import asyncio
 import os
 
 from telebot.async_telebot import AsyncTeleBot
+from keyboards import get_year_selection_keyboard
+from massages import start_message
 
 bot = AsyncTeleBot(os.environ['TEST_BOT_TOKEN'])
 
@@ -11,8 +13,9 @@ bot = AsyncTeleBot(os.environ['TEST_BOT_TOKEN'])
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 async def send_welcome(message):
-    text = 'Привет! \nЧем я могу вам помочь?'
-    await bot.reply_to(message, text)
+    text = start_message
+    markup = get_year_selection_keyboard()
+    await bot.reply_to(message, text, reply_markup=markup)
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
