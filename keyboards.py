f0fb8c888078de6eece57f467e91d7ef0e04f746
MishaNarfu <misha.nya.narf@gmail.com>
2025-12-01 18:35:19 +0300

    change get_event_selection_keyboard

--- keyboards.py
+++ keyboards.py
@@ -1,24 +1,35 @@
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 from telebot import types
 
+
+years = [2024, 2025]
+events = {}
+event_2024 = ['конференция', 'турнир трёх наук']
+
+for year in years:
+    events[year] = event_2024
+
 def get_year_selection_keyboard():
     year_selection_keyboard = types.InlineKeyboardMarkup()
 
-    years = [2024, 2025]
-    buttons =[]
+    buttons_year =[]
 
     for year in years:
-        buttons.append(types.InlineKeyboardButton(str(year), callback_data='year_'+str(year)))
+        buttons_year.append(types.InlineKeyboardButton(str(year), callback_data='year_'+str(year)))
 
-    year_selection_keyboard.row(*buttons)
+    year_selection_keyboard.row(*buttons_year)
 
     return year_selection_keyboard
 
-def get_event_selection_keyboard():
-    event_selection_keyboard = InlineKeyboardMarkup()
+def get_event_selection_keyboard(year):
+    event_selection_keyboard = types.InlineKeyboardMarkup()
 
-    event_selection_keyboard.row(
-    InlineKeyboardButton("конференция", callback_data="event_conf")
-    )
+
+    buttons_event = []
+
+    for event in events[year]:
+        buttons_event.append(types.InlineKeyboardButton(str(event), callback_data='event_' + str(event)))
+
+    event_selection_keyboard.add(*buttons_event)
 
     return event_selection_keyboard
