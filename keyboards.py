fc27b4b18058caa72ec0655af3a3c7b2dfd6bf1b
MishaNarfu <misha.nya.narf@gmail.com>
2026-01-26 14:35:21 +0300

    create admin_keyboard

--- keyboards.py
+++ keyboards.py
@@ -1,13 +1,10 @@
-from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 from telebot import types
 
 
 years = [2024, 2025]
-events = {}
-event_2024 = ['конференция', 'турнир трёх наук']
-
-for year in years:
-    events[year] = event_2024
+events = {2024:['конфепенция '], 2025:['турнир трёх наук']}
+admin_function = ['добавить год','добавить мероприяте']
 
 def get_year_selection_keyboard():
     year_selection_keyboard = types.InlineKeyboardMarkup()
@@ -17,7 +14,8 @@ def get_year_selection_keyboard():
     for year in years:
         buttons_year.append(types.InlineKeyboardButton(str(year), callback_data='year_'+str(year)))
 
-    year_selection_keyboard.row(*buttons_year)
+    for i in buttons_year:
+        year_selection_keyboard.add(i)
 
     return year_selection_keyboard
 
@@ -30,6 +28,23 @@ def get_event_selection_keyboard(year):
     for event in events[year]:
         buttons_event.append(types.InlineKeyboardButton(str(event), callback_data='event_' + str(event)))
 
-    event_selection_keyboard.add(*buttons_event)
+    for i in buttons_event:
+        event_selection_keyboard.add(i)
 
     return event_selection_keyboard
+
+def admin_keyboard():
+    admin_panel = types.InlineKeyboardMarkup()
+
+    admin_button = []
+
+    for function in admin_function:
+        admin_button.append(types.InlineKeyboardButton(str(function), callback_data = 'admin_' + str(function)))
+
+    for i in admin_button:
+        admin_panel.add(i)
+
+    return(admin_panel)
+
+def add_year(year):
+    years.append(year)
