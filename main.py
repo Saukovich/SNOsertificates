a8dc2286e9372b1d59b7771a44961c63100e0934
MishaNarfu <misha.nya.narf@gmail.com>
2026-01-26 14:36:05 +0300

    create admin_welcome

--- main.py
+++ main.py
@@ -23,6 +23,26 @@ async def send_welcome(message):
     year_selection_keyboard = get_year_selection_keyboard()
     await bot.reply_to(message, text, reply_markup=year_selection_keyboard)
 
+@bot.message_handler(commands='admin')
+async def admin_welcome(message):
+    """
+    Вызывает админ-панель
+
+    :param message: сообщение, на которое отвечает бот
+    :return: None
+    """
+    text = start_admin_message
+    admin_panel = admin_keyboard()
+    await bot.reply_to(message, text,  reply_markup=admin_panel)
+
+@bot.callback_query_handler(func=lambda call:'admin_добавить год')
+async def handle_admin_command (call):
+    text = add_year_message
+    @bot.message_handler(func=lambda message: message.text.startswith("20"))
+    async def handle_add_year(message):
+        add_year(str(message))
+    await bot.send_message(call.message.chat.id, text)
+
 @bot.callback_query_handler(func=lambda call:call.data.startswith('year_'))
 async def handle_year (call):
     """
@@ -36,16 +56,17 @@ async def handle_year (call):
     await bot.send_message(call.message.chat.id, text, reply_markup=event_selection_keyboard)
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
+"""@bot.message_handler(func=lambda message: True)
 async def echo_unknown_message(message):
-    """
+    
     Отвечает на неопознанные сообщения
 
     :param message: сообщение, на которое отвечает бот
     :return: None
-    """
+    
     text = unknown_message
     await bot.reply_to(message, text)
+    """
 
 if __name__ == '__main__':
     asyncio.run(bot.polling())
